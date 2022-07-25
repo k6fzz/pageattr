29f96886600ffeadda36baf7b51eee5fcfe22e80
Jon Montgomery <jonbmontgomery@gmail.com>
2022-07-24 22:29:09 -0700

    add pml4 change support

--- pageattr.c
+++ pageattr.c
@@ -41,13 +41,15 @@ void drawAddressView(WINDOW* window, uint64_t address)
     index4 = vaddr & 0x1FF;
 
     wclear(window);
+
     box(window, 0, 0);
     mvwprintw(window, 0, 1, "Address");
     mvwprintw(window, 1, 1, "Address: %p", address);
-    mvwprintw(window, 2, 1, "PML Index:   %d", index4);
-    mvwprintw(window, 3, 1, "PDPTE Index: %d", index3);
-    mvwprintw(window, 4, 1, "PDE Index:   %d", index2);
-    mvwprintw(window, 5, 1, "PTE Index:   %d", index1);
+    mvwprintw(window, 2, 1, "DEC: %lld", address);
+    mvwprintw(window, 4, 1, "PML Index:   %d", index4);
+    mvwprintw(window, 5, 1, "PDPTE Index: %d", index3);
+    mvwprintw(window, 6, 1, "PDE Index:   %d", index2);
+    mvwprintw(window, 7, 1, "PTE Index:   %d", index1);
 
     wrefresh(window);
 }
@@ -68,14 +70,43 @@ int main(int argc, char** argv)
 
     uint64_t address = 0;
 
+    uint64_t index4, index3, index2, index1;
+
+    
+
+    
     while(1)
     {
                 
+    uint64_t vaddr = address;
+    vaddr >>= 12;
+    index1 = vaddr & 0x1FF;
+    vaddr >>= 9;
+    index2 = vaddr & 0x1FF;
+    vaddr >>= 9;
+    index3 = vaddr & 0x1FF;
+    vaddr >>= 9;
+    index4 = vaddr & 0x1FF;
+
+    wclear(AddressView);
+
+    box(AddressView, 0, 0);
+    mvwprintw(AddressView, 0, 1, "Address");
+    mvwprintw(AddressView, 1, 1, "Address: %p", address);
+    mvwprintw(AddressView, 2, 1, "DEC: %lld", address);
+    mvwprintw(AddressView, 4, 1, "PML Index:   %d", index4);
+    mvwprintw(AddressView, 5, 1, "PDPTE Index: %d", index3);
+    mvwprintw(AddressView, 6, 1, "PDE Index:   %d", index2);
+    mvwprintw(AddressView, 7, 1, "PTE Index:   %d", index1);
+
+    wrefresh(AddressView);  
+        
+
+
+        
         
         //Draw Windows
-        drawAddressView(AddressView, address);
-
-
+        //drawAddressView(AddressView, address);
         int input = getch();
         switch(input)
         {
@@ -84,9 +115,15 @@ int main(int argc, char** argv)
             case 'Q':
                 goto exit;
             case KEY_IC:
-                address += 0x40000000;
+                address += 0x8000000000;
                 break;
             case KEY_DC:
+                address -= 0x8000000000;
+                break;
+            case KEY_HOME:
+                address += 0x40000000;
+                break;
+            case KEY_END:
                 address -= 0x40000000;
                 break;
             case KEY_PPAGE:
